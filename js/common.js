91cb08dc29ec4e52023c2f4ceeb11931df0ea232
gren-yyy <124562558+gren-yyy@users.noreply.github.com>
2023-02-07 17:51:35 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -9,33 +9,36 @@ var ww = $(window).width()
         //         $(this).toggleClass('on')
         //     }
         // })
-        $('#header .row .depth1 > li').on('mouseover mouseout', function(){
-            if ($('html').hasClass('pc')) {
-                $(this).toggleClass('on').siblings().removeClass('on')
-            }
-        })
-        $('#header .row .depth2 > li').on('mouseover mouseout', function(){
-            if($('html').hasClass('pc')) {
-                $(this).toggleClass('on')
-                return false
-            }
-        })
-
-        $('#header .row .open > i').on('click', function(){
-            if($('html').hasClass('mobile')) {
-                $(this).parent().toggleClass('on')
-                return false
-            }
-        })
-
-        $('#header .row .depth1 > li').on('mouseover mouseout', function(){
-            if($('html').hasClass('mobile')) {
-                $(this).toggleClass('on')
-            }
-        })
+        // $('#header .row .depth1 > li').on('mouseover mouseout', function(){
+        //     if ($('html').hasClass('pc')) {
+        //         $(this).toggleClass('on').siblings().removeClass('on')
+        //     }
+        // })
+        $('#header .row .depth1 > li').hover(
+            function() {
+                if ($('html').hasClass('pc')) {
+                            $(this).addClass('on')
+                        } 
+            },
+            function() { if ($('html').hasClass('pc')) {
+                $(this).removeClass('on')
+            } 
+                }
+        )
 
         $('#header .row .depth2 > li').on('mouseover mouseout', function(){
-            if($('html').hasClass('mobile')) {
                 $(this).toggleClass('on')
+        })
+
+        $('#header .row .open').on('click', function(){
+                $(this).next().slideToggle()
+        })
+        // open 다음 동생인 navbox
+        $('#header .row .depth1 > li > a').on('click', function(){
+            if($('html').hasClass('mobile')) {
+                $(this).next().slideToggle()
+                $(this).parent().siblings().find('.depth2').slideUp()
+                return false
             }
+           // a 다음 동생인  denth2
         })
